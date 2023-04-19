dacea29d7a4305a09c31b50460414321e5f4f26f
ywwwtseng <ywwwtseng@gmail.com>
2023-04-19 11:00:35 +0800

    feat: set token in the cookies with httponly when user request login api

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -7,7 +7,6 @@ export async function POST(request: Request) {
 
   return new Response(JSON.stringify({ result: 'ok', data: { access_token: body.access_token } }), {
     status: 200,
-    // headers: { 'Set-Cookie': `token=123; Max-Age=86400; Path=/; Secure; HttpOnly` },
-    headers: { 'Set-Cookie': `token=${body.access_token}; Max-Age=86400; Path=/; Secure` }
+    headers: { 'Set-Cookie': `token=${body.access_token}; Max-Age=86400; Path=/; Secure; HttpOnly` },
   });
 }
